a7eb77dd2f0ecec408faf4c6fc1b7b73aa8a9da5
Willian <w.goncalves91@gmail.com>
2019-09-03 15:18:55 -0300

    [feat] Add scripts to detect changes on etherpad plugins and tag them

---
+++ scripts/detect-changes.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo 'repos with uncommitted changes: '
+
+for dir in * ; do
+  if [[ -d $dir  ]] && [[ -d $dir/.git ]]; then
+    (
+      cd $dir
+      # do we have any change on repo?
+      if [ 1 -ne `git status | grep 'nothing to commit' | wc -l` ]; then
+        echo $dir
+        git status -s
+      fi
+    );
+  fi
+done
+
+echo
+echo 'repos with unpushed changes: '
+
+for dir in * ; do
+  if [[ -d $dir  ]] && [[ -d $dir/.git ]]; then
+    (
+      cd $dir
+      # do we have any unpushed commit on repo?
+      if [ 0 -ne `git status | grep 'Your branch is ahead of' | wc -l` ]; then
+        echo $dir
+      fi
+    );
+  fi
+done
+
+echo
+echo 'repos on feature-branches: '
+
+for dir in * ; do
+  if [[ -d $dir  ]] && [[ -d $dir/.git ]]; then
+    (
+      cd $dir
+      # are we on master branch?
+      if [ 1 -ne `git status | grep 'On branch master' | wc -l` ]; then
+        echo $dir": "$(git rev-parse --abbrev-ref HEAD)
+      fi
+    );
+  fi
+done
